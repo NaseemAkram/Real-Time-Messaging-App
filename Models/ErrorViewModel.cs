f7e5fb5a395e54ef7bc68ac26f5d8a3f19e46c58
NaseemAkram <100962067+NaseemAkram@users.noreply.github.com>
2023-04-21 15:43:36 +0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_RealTimeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
